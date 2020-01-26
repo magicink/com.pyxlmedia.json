2224231723950fa18d5d70035fbb77dd45872091
Brandon Tom <magicink@gmail.com>
2020-01-25 19:21:57 -0800

    Initial commit

---
+++ Runtime/JsonCollection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PyxlMedia.JSON
+{
+    /// <summary>
+    /// A tool that helps Unity parse JSON arrays.
+    /// </summary>
+    public static class JsonCollection
+    {
+        [Serializable]
+        private class Wrapper<T>
+        {
+            public T[] items;
+        }
+            
+        /// <summary>
+        /// Converts an JSON array into a collection.
+        /// </summary>
+        /// <param name="json">The raw JSON string containing the data array.</param>
+        /// <typeparam name="T">The model the JSON data is being converted to. Must be Serializable.</typeparam>
+        /// <returns>A collection of T</returns>
+        public static IEnumerable<T> FromJson<T>(string json)
+        {
+            json = $"{{\"items\":{json}}}";
+            var wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+            return wrapper.items;
+        }
+    }
+}
+
